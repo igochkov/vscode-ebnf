f60187fd980e4f9ee5159b4aacc213d14403e4a7
igochkov <ivaylo@gochkov.com>
2023-06-17 22:44:59 +0200

    Bundling the extension code; Removal of unnecessary files from the deployment package.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'webworker',
+  entry: './client/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
