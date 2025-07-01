1da0cd655865a2429998a3f8725264b68c041720
igochkov <ivaylo@gochkov.com>
2025-07-01 22:02:30 +0200

    unit tests

---
+++ tests/SyntaxTests.ts
@@ -0,0 +1,57 @@
+import { EBNFParser, SyntaxContext, SyntaxRuleContext } from "../src/parser/EBNFParser";
+import { collectErrorNodes, parseRule } from "./utils";
+
+// syntax
+// 	: syntaxRule+ EOF
+// 	;
+
+test('should find single syntax rule', () => {
+    const input = "syntacticPrimary = optionalSequence | repeatedSequence;";
+    const context: SyntaxContext = parseRule('syntax', input);
+
+    const syntaxRules = context.syntaxRule();
+
+    expect(syntaxRules).not.toBeNull();
+    expect(syntaxRules).toHaveLength(1);
+    expect(syntaxRules[0]?.ruleIndex).toEqual(EBNFParser.RULE_syntaxRule);
+    expect(collectErrorNodes(syntaxRules[0])).toHaveLength(0);
+});
+
+test('should find 2 syntax rules', () => {
+    const input = `
+    program
+        = { item };
+
+    item
+        = fn-defn
+        | fn-decl;
+    `;
+    const context: SyntaxContext = parseRule('syntax', input);
+    const syntaxRules = context.syntaxRule();
+
+    expect(syntaxRules).not.toBeNull();
+    expect(syntaxRules).toHaveLength(2);
+    expect(syntaxRules[0]?.ruleIndex).toEqual(EBNFParser.RULE_syntaxRule);
+    expect(syntaxRules[1]?.ruleIndex).toEqual(EBNFParser.RULE_syntaxRule);
+    expect(collectErrorNodes(syntaxRules[0])).toHaveLength(0);
+    expect(collectErrorNodes(syntaxRules[1])).toHaveLength(0);
+});
+
+test('issue #18', () => {
+    const input = `
+    special-symbol = '+' | '-' | '*' | '/' | '¬' | '=' | '<' | '>' | '[' | ']' | '.' | ',' | ':' |
+                 ';' | '^' | '(' | ')' | '<>' | '<=' | '>=' | ':=' | '..' | 
+                 'and' | 'array' | 'begin' | 'case' | 'const' | 'div' | 'do' | 'downto' | 'else' |
+              'end' | 'file' | 'for' | 'function' | 'goto' | 'if' | 'in' | 'label' | 'mod' |
+              'nil' | 'not' | 'of' | 'or' | 'packed' | 'procedure' | 'program' | 'record' |
+              'repeat' | 'set' | 'then' | 'to' | 'type' | 'until' | 'var' | 'while' | 'with' ;
+    `;
+
+    const context: SyntaxContext = parseRule('syntax', input);
+    const syntaxRules = context.syntaxRule();
+
+    syntaxRules.forEach((syntaxRule: SyntaxRuleContext) => {
+        expect(syntaxRule?.ruleIndex).toEqual(EBNFParser.RULE_syntaxRule);
+        expect(collectErrorNodes(syntaxRule)).toHaveLength(0);
+    });
+});
