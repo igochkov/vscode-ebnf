5d7f8821540d1d64bce71d924641d23602786aaf
igochkov <ivaylo@gochkov.com>
2025-07-16 21:48:18 +0200

    unit tests fix

--- tests/SyntacticFactorTests.ts
+++ tests/SyntacticFactorTests.ts
@@ -8,8 +8,8 @@ import { collectErrorNodes, parseRule } from "./utils";
 test('should find only syntactic primary', () => {
     const input = "'terminal string'";
     const context: SyntacticFactorContext = parseRule('syntacticFactor', input);
-    const integer = context.INTEGER();
-    const repetitionSymbol = context.REPETITION_SYMBOL();
+    const integer = context.integerWithComments();
+    const repetitionSymbol = context.repetitionSymbolWithComments();
     const syntacticPrimary = context.syntacticPrimary();
 
     expect(integer).toBeNull();
@@ -24,15 +24,15 @@ test('should find only syntactic primary', () => {
 test('should find full semantic factor - no space after integer', () => {
     const input = "2* 'terminal string'";
     const context: SyntacticFactorContext = parseRule('syntacticFactor', input);
-    const integer = context.INTEGER();
-    const repetitionSymbol = context.REPETITION_SYMBOL();
+    const integer = context.integerWithComments();
+    const repetitionSymbol = context.repetitionSymbolWithComments();
     const syntacticPrimary = context.syntacticPrimary();
 
     expect(integer).not.toBeNull();
-    expect(integer?.symbol.type).toEqual(EBNFParser.INTEGER);
+    expect(integer?.INTEGER()?.symbol.type).toEqual(EBNFParser.INTEGER);
 
     expect(repetitionSymbol).not.toBeNull();
-    expect(repetitionSymbol?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
+    expect(repetitionSymbol?.REPETITION_SYMBOL()?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
 
     expect(syntacticPrimary).not.toBeNull();
     expect(syntacticPrimary?.ruleIndex).toEqual(EBNFParser.RULE_syntacticPrimary);
@@ -43,15 +43,15 @@ test('should find full semantic factor - no space after integer', () => {
 test('should find full semantic factor - with space after integer', () => {
     const input = "2 * 'terminal string'";
     const context: SyntacticFactorContext = parseRule('syntacticFactor', input);
-    const integer = context.INTEGER();
-    const repetitionSymbol = context.REPETITION_SYMBOL();
+    const integer = context.integerWithComments();
+    const repetitionSymbol = context.repetitionSymbolWithComments();
     const syntacticPrimary = context.syntacticPrimary();
 
     expect(integer).not.toBeNull();
-    expect(integer?.symbol.type).toEqual(EBNFParser.INTEGER);
+    expect(integer?.INTEGER()?.symbol.type).toEqual(EBNFParser.INTEGER);
 
     expect(repetitionSymbol).not.toBeNull();
-    expect(repetitionSymbol?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
+    expect(repetitionSymbol?.REPETITION_SYMBOL()?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
 
     expect(syntacticPrimary).not.toBeNull();
     expect(syntacticPrimary?.ruleIndex).toEqual(EBNFParser.RULE_syntacticPrimary);
@@ -62,35 +62,18 @@ test('should find full semantic factor - with space after integer', () => {
 test('should find full semantic factor - no spaces', () => {
     const input = "2*'terminal string'";
     const context: SyntacticFactorContext = parseRule('syntacticFactor', input);
-    const integer = context.INTEGER();
-    const repetitionSymbol = context.REPETITION_SYMBOL();
+    const integer = context.integerWithComments();
+    const repetitionSymbol = context.repetitionSymbolWithComments();
     const syntacticPrimary = context.syntacticPrimary();
 
     expect(integer).not.toBeNull();
-    expect(integer?.symbol.type).toEqual(EBNFParser.INTEGER);
+    expect(integer?.INTEGER()?.symbol.type).toEqual(EBNFParser.INTEGER);
 
     expect(repetitionSymbol).not.toBeNull();
-    expect(repetitionSymbol?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
+    expect(repetitionSymbol?.REPETITION_SYMBOL()?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
 
     expect(syntacticPrimary).not.toBeNull();
     expect(syntacticPrimary?.ruleIndex).toEqual(EBNFParser.RULE_syntacticPrimary);
 
     expect(collectErrorNodes(syntacticPrimary)).toHaveLength(0);
 });
-
-// test('should fail wrong syntax', () => {
-//     const input = "2*4*'terminal string'";
-//     const context: SyntacticFactorContext = parseRule('syntacticFactor', input);
-//     const integer = context.INTEGER();
-//     const repetitionSymbol = context.REPETITION_SYMBOL();
-//     const syntacticPrimary = context.syntacticPrimary();
-
-//     expect(integer).not.toBeNull();
-//     expect(integer?.symbol.type).toEqual(EBNFParser.INTEGER);
-
-//     expect(repetitionSymbol).not.toBeNull();
-//     expect(repetitionSymbol?.symbol.type).toEqual(EBNFParser.REPETITION_SYMBOL);
-
-//     expect(syntacticPrimary).not.toBeNull();
-//     expect(collectErrorNodes(syntacticPrimary).length).toBeGreaterThan(0);
-// });
